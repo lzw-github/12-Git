49092621613bbc22b8a31416777665f0eab7b02f
lzw <3323592816@qq.com>
2021-06-28 09:47:37 +0800

    IDEA第五次提交:增空格和删除文件

--- ch01-git-test/src/main/java/com/bjpowernode/GitTest.java
+++ ch01-git-test/src/main/java/com/bjpowernode/GitTest.java
@@ -3,7 +3,7 @@ package com.bjpowernode;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("HelloGit2!");
+        System.out.println("Hello Git2!");
         System.out.println("Hello Git3");
     }
 }
--- ch01-git-test/src/main/java/com/bjpowernode/GitTest2.java
+++
@@ -1,7 +0,0 @@
-package com.bjpowernode;
-
-public class GitTest2 {
-    public static void main(String[] args) {
-        System.out.println("Test2-11111");
-    }
-}
