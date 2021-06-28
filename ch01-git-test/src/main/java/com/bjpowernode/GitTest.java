d9746abbf756932f350412a485189de8713a81cf
lzw <3323592816@qq.com>
2021-06-28 11:09:27 +0800

    IDEA第六次提交:删空格和删语句

--- ch01-git-test/src/main/java/com/bjpowernode/GitTest.java
+++ ch01-git-test/src/main/java/com/bjpowernode/GitTest.java
@@ -3,7 +3,6 @@ package com.bjpowernode;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("Hello Git2!");
-        System.out.println("Hello Git3");
+        System.out.println("HelloGit2!");
     }
 }
