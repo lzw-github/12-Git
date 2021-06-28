f5ad11cc06075d3314430887136f2eee36689f66
lzw <3323592816@qq.com>
2021-06-28 09:43:02 +0800

    IDEA第四次提交:改和新增文件

--- ch01-git-test/src/main/java/com/bjpowernode/GitTest.java
+++ ch01-git-test/src/main/java/com/bjpowernode/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("HelloGit2!");
+        System.out.println("Hello Git3");
     }
 }
---
+++ ch01-git-test/src/main/java/com/bjpowernode/GitTest2.java
@@ -0,0 +1,7 @@
+package com.bjpowernode;
+
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("Test2-11111");
+    }
+}
