c53cf74ba67eb01a0cb75ede4e184db383642cac
lzw <3323592816@qq.com>
2021-06-28 22:27:51 +0800

    Revert:master从second到first

--- ch01-git-test/src/main/java/com/bjpowernode/GitTest.java
+++ ch01-git-test/src/main/java/com/bjpowernode/GitTest.java
@@ -9,6 +9,5 @@ public class GitTest {
         System.out.println("hot-fix test1");
         System.out.println("hot-fix test2");
         System.out.println("master test1");
-        System.out.println("master test2");
     }
 }
